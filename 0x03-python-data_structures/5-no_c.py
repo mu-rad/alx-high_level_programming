277643d89c0dafed27fffdbd518ca5187b40f598
mu-rad <muradabdulrahman533@gmail.com>
2024-01-08 09:02:46 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string [:]:
+    if my_string[:]:
         nstring = my_string.translate({ord("c"): None})
         nstring2 = my_string.translate({ord("C"): None})
         return nstring2
